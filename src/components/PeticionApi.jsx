8418f4eced90506d8722fd104abe81aea9ca3deb
Luis Puello <luiscapupa@gmail.com>
2023-05-21 14:56:52 -0500

    mostrando titulo y personajes

--- src/components/PeticionApi.jsx
+++ src/components/PeticionApi.jsx
@@ -6,17 +6,36 @@ const PeticionApi = () => {
 
   const obtenerPersonajes = async () => {
     try {
-        const res = await fetch(`http://api.disneyapi.dev/character?page=1&pageSize=50`)
+        const res = await fetch(`http://api.disneyapi.dev/character?page=${paginacion}&pageSize=50`)
         const {data} = await res.json()
-        console.log(data);
+        setPersonajes(data)
     } catch (error) {
         console.log(error);
     }
   }
+  
 
 
   return (
-    <button onClick={obtenerPersonajes}>Obtener personajes</button>
+    <div >
+      <h1 >Personajes de Disney</h1>
+      <hr />
+      <div>
+        <button  onClick={obtenerPersonajes}>Obtener personajes</button>
+      </div>
+      
+      
+      {
+        personajes.map(({_id,name,imageUrl}) => (
+            <div key={_id}>
+                <div >
+                  <img src= {imageUrl} alt={name} />
+                  <h4>{_id} - {name}</h4>
+                </div> 
+            </div>
+        ))
+      }
+    </div>
   );
 }
 
